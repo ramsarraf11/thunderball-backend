5f2cbe22f339d65598743622e468a3493a27ae8f
Ram Sarraf <91001336+ramsarraf11@users.noreply.github.com>
2023-09-24 00:28:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const path = require('path');
 const cors = require('cors');
 const PORT = 3000;
 
-app.use(cors());
+
 app.use(express.json());
+app.use(cors());
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*'); // Replace with your actual origin
